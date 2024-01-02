4ddd8893fbf3b915275da3ca4e48cd7439724ab3
Małgorzata Drąg <malgorzata.drag@spoton.com>
2024-01-02 23:12:55 +0100

    fix randomize new item id

--- backend/internal/services/merchandise.go
+++ backend/internal/services/merchandise.go
@@ -1,15 +1,25 @@
 package services
 
-import "emptyslot/internal/models"
+import (
+	"emptyslot/internal/models"
+	"github.com/google/uuid"
+	"strconv"
+)
 
 type MerchandiseRequest struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
-func (mr *MerchandiseRequest) ToModel() *models.Merchandise {
+func (mr *MerchandiseRequest) ToModel(generateNewID bool) *models.Merchandise {
+	var id string
+	if generateNewID {
+		id = strconv.Itoa(int(uuid.New().ID()))
+	} else {
+		id = mr.ID
+	}
 	return &models.Merchandise{
-		ID:   mr.ID,
+		ID:   id,
 		Name: mr.Name,
 	}
 }
