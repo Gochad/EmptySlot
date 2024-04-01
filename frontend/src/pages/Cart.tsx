e9985f0118b9cc509b51aceb6fd283e244c4ba4d
Małgorzata Drąg <malgorzata.drag@spoton.com>
2024-04-01 13:21:05 +0200

    cart ui change

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { List, ListItem } from '@mui/material';
 import Navbar from "../components/Navbar";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+
 
 export default function Cart() {
     const items = [
@@ -13,15 +13,15 @@ export default function Cart() {
             id: 1,
             name: 'item1',
             description: 'desc1',
-            merchandises: ['Merchandiseid1', 'Merchandiseid2'],
-            isReserved: false,
+            price: 1,
+            confirmed: false,
         },
         {
             id: 2,
             name: 'item2',
             description: 'desc2',
-            merchandises: ['Merchandiseid3', 'Merchandiseid4'],
-            isReserved: true,
+            price: 1,
+            confirmed: false,
         },
     ];
 
@@ -30,7 +30,7 @@ export default function Cart() {
             <Navbar />
             <Container maxWidth="md">
                 <Typography variant="h4" component="h1" gutterBottom>
-                    Merchandises in your cart to reserve
+                    Merchandises to reserve
                 </Typography>
                 <Grid container spacing={2}>
                     {items.map((item) => (
@@ -42,15 +42,11 @@ export default function Cart() {
                                 <Typography sx={{ mb: 1.5 }} color="text.secondary">
                                     {item.description}
                                 </Typography>
-                                <List>
-                                    {item.merchandises.map((merchandise, index) => (
-                                        <ListItem key={index}>
-                                            {merchandise}
-                                        </ListItem>
-                                    ))}
-                                </List>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    {item.price}
+                                </Typography>
                                 <Typography variant="body2">
-                                    {item.isReserved ? 'Reserved' : 'Available'}
+                                    {item.confirmed ? 'Reserved' : 'Available'}
                                 </Typography>
                             </CardContent>
                         </Card>
